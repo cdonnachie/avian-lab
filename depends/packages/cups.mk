167860ba5731d738168dc4bb81599c2cb99d736e
cdonnachie <craig.donnachie@gmail.com>
2026-03-18 07:35:21 -0400

    Fix cups depends recipe for cross-compilation
    - Use autoconf macro instead of manual ./configure to pass cross-compiler toolchain
    
    - Move configure flags to config_opts via set_vars
    
    - Use POSIX-portable sed (temp file + mv) instead of sed -i

--- depends/packages/cups.mk
+++ depends/packages/cups.mk
@@ -5,37 +5,28 @@ $(package)_download_path=https://github.com/OpenPrinting/cups/releases/download/
 $(package)_file_name=cups-$($(package)_version)-source.tar.gz
 $(package)_sha256_hash=dd54228dd903526428ce7e37961afaed230ad310788141da75cebaa08362cf6c
 
-$(package)_dependencies =
+$(package)_dependencies=
+
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared --enable-static
+  $(package)_config_opts+=--disable-dbus --disable-libpaper --disable-pam
+  $(package)_config_opts+=--disable-gssapi --disable-avahi --without-systemd
+  $(package)_config_opts+=--with-components=libcups --with-tls=no
+endef
 
 define $(package)_preprocess_cmds
-	sed -i '' 's|#ifdef HAVE_OPENSSL|#if 0|g' cups/hash.c && \
-	sed -i '' 's|#else // HAVE_GNUTLS|#elif 0|g' cups/hash.c
+  sed 's|#ifdef HAVE_OPENSSL|#if 0|g' cups/hash.c > cups/hash.c.tmp && mv cups/hash.c.tmp cups/hash.c && \
+  sed 's|#else // HAVE_GNUTLS|#elif 0|g' cups/hash.c > cups/hash.c.tmp && mv cups/hash.c.tmp cups/hash.c
 endef
 
 define $(package)_config_cmds
-	./configure \
-	  --prefix=$(host_prefix) \
-	  --exec-prefix=$(host_prefix) \
-	  --libdir=$(host_prefix)/lib \
-	  --includedir=$(host_prefix)/include \
-	  --with-pkgconfpath=$(host_prefix)/lib/pkgconfig \
-	  --host=$(host) \
-	  --disable-shared \
-	  --enable-static \
-	  --disable-dbus \
-	  --disable-libpaper \
-	  --disable-pam \
-	  --disable-gssapi \
-	  --disable-avahi \
-	  --without-systemd \
-	  --with-components=libcups \
-	  --with-tls=no
+  $($(package)_autoconf)
 endef
 
 define $(package)_build_cmds
-	$(MAKE)
+  $(MAKE)
 endef
 
 define $(package)_stage_cmds
-	$(MAKE) DESTDIR=$($(package)_staging_dir) install
+  $(MAKE) DESTDIR=$($(package)_staging_dir) install
 endef
