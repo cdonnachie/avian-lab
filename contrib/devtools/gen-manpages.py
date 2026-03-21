7aaed47215a60a32217f00927acd1325f0035a29
cdonnachie <craig.donnachie@gmail.com>
2026-03-21 16:56:49 -0400

    Fix man page install names and missing libexec in guix build
    - InstallBinaryComponent.cmake: Use OUTPUT_NAME property to find man
      pages, so avian*.1 files are installed instead of bitcoin*.1
    - gen-manpages.py: Update BINARIES list to avian binary names
    - build.sh: Only include libexec dir in find if it exists (fixes
      error when IPC is disabled, e.g. Windows builds)
    - Rename doc/man/bitcoin*.1 files to avian*.1

--- contrib/devtools/gen-manpages.py
+++ contrib/devtools/gen-manpages.py
@@ -10,13 +10,13 @@ import tempfile
 import argparse
 
 BINARIES = [
-'bin/bitcoin',
-'bin/bitcoind',
-'bin/bitcoin-cli',
-'bin/bitcoin-tx',
-'bin/bitcoin-wallet',
-'bin/bitcoin-util',
-'bin/bitcoin-qt',
+'bin/avian',
+'bin/aviand',
+'bin/avian-cli',
+'bin/avian-tx',
+'bin/avian-wallet',
+'bin/avian-util',
+'bin/avian-qt',
 ]
 
 parser = argparse.ArgumentParser(
